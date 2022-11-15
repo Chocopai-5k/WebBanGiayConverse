53e7e743e1b6195684a3d32dd8b6c8583cbcc758
ManhBeo <uETT2hpX6j6TJWR>
2022-11-15 22:40:10 +0700

    Add project files.

---
+++ WebBanGiayConverse/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanGiayConverse.Models;
+
+namespace WebBanGiayConverse.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        ShopGiayConverseEntities db = new ShopGiayConverseEntities();
+        public ActionResult Index()
+        {
+            var lstConverse = db.SanPhams.Where(n => n.IDLoaiSanPham == 1);
+            ViewBag.ListConverse = lstConverse;
+
+            var lstVan = db.SanPhams.Where(n => n.IDLoaiSanPham == 2);
+            ViewBag.ListVan = lstVan;
+            return View();
+        }
+        public ActionResult MenuPartial()
+        {
+            var lstSP = db.SanPhams;
+            return PartialView(lstSP);
+        }
+    }
+}
---
+++ WebBanGiayConverse/Controllers/SanPhamController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanGiayConverse.Models;
+
+namespace WebBanGiayConverse.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+        ShopGiayConverseEntities db = new ShopGiayConverseEntities();
+        //Tạo 1 partial view sản phẩm để hiên thị theo 2 style khac nhau
+        [ChildActionOnly]
+        public ActionResult SanPhamStyle1()
+        {
+            return PartialView();
+        }
+        //Xây dựng trang xem chi tiết
+        public ActionResult XemChiTiet(int? id)
+        {
+            //Kiểm tra tham số truyền vào có rỗng hay không
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            }
+            SanPham sp = db.SanPhams.SingleOrDefault(n => n.ID == id);
+            if(sp == null)
+            {
+                return HttpNotFound();
+
+            }
+            return View(sp);
+        }
+        //Load sản phẩm theo mã loại sản phẩm
+       public ActionResult SanPham(int? IDLoaiSP)
+        {
+            if(IDLoaiSP == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            }
+            var lstSP = db.SanPhams.Where(n => n.IDLoaiSanPham == IDLoaiSP);
+            if(lstSP.Count() == 0)
+            {
+                //Thông báo nếu không có sản phẩm 
+                return HttpNotFound();
+            }
+            return View(lstSP);
+        }
+    }
+}
